03e053e9f7cc5c4e10cb6862f718261b8fc228e3
subrat1985 <subrat.pintu1@gmail.com>
2021-10-18 20:23:34 +0530

    uri added in prop

--- src/main/java/com/emp/controller/EmpController.java
+++ src/main/java/com/emp/controller/EmpController.java
@@ -2,6 +2,7 @@ package com.emp.controller;
 
 import com.emp.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +16,10 @@ import java.util.Map;
 public class EmpController {
     @Autowired
     RestTemplate restTemplate;
+    @Value("${uri_emp_mgm}")
+    String URI_USERS_ID;
     @GetMapping(value = "/findEmpId")
     public Employee findById(int id){
-        final String URI_USERS_ID = "http://localhost:8080/employee/findId/{id}";
         Map<String, Integer> params = new HashMap<String, Integer>();
         params.put("id", id);
         Employee employee = restTemplate.getForObject(URI_USERS_ID, Employee.class, params);
